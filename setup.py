a8017af06c88a1f49812919c6b88eb6cea06443d
Tomiinek <tom@neqindi.cz>
2022-01-14 09:43:43 +0100

    Fixed data files.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import glob
 from pathlib import Path
 from setuptools import setup, find_packages
 
@@ -7,11 +8,14 @@ install_requires = (project_root / 'requirements.txt').read_text().splitlines()
 
 print(find_packages())
 
+data_files = glob.glob('data/**/*.json') + glob.glob('data/*.json')
+
 setup(
     name="mwzeval",
     author="Tomas Nekvinda",
     version="0.0.2",
     packages=find_packages(),
+    package_data={'mwzeval': data_files},
     python_requires=">=3.6",
     install_requires=install_requires,
 ) 
